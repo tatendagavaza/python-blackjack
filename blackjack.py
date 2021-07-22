40be450ebaa55c6ad7a45016f2ea944bcc550239
Tatenda Gavaza <tgavaza2515@gmail.com>
2021-07-22 14:33:29 -0700

    Added scoring and Win/Lose states

--- blackjack.py
+++ blackjack.py
@@ -50,11 +50,13 @@ dealer_cards = []
 def game():
     user_hand.append(random.choice(deck))
     user_hand.append(random.choice(deck))
+    user_cards = get_cards(user_hand)
     user_score = get_score(user_hand)
 
     dealer_hand.append(random.choice(deck))
     dealer_hand.append(random.choice(deck))
     dealer_score = get_score(dealer_hand)
+    dealer_cards = get_cards(dealer_hand)
 
     should_continue = True
     if dealer_score == 21:
@@ -91,9 +93,27 @@ def game():
                         value[1] = 1
     dealer_score = get_score(dealer_hand)
 
+    print("Your final Hand:")
+    print(" ".join(str(card) for card in user_cards))
+
+    print("Dealer final Hand:")
+    print(" ".join(str(card) for card in dealer_cards))
+
+    print("Your Final Score: {}".format(user_score))
+    print("Dealer Final Score: {}".format(dealer_score))
 
 
     if user_over_21:
-        print("You went over 21. You Lose")
+        print("You went over 21.\nYou Lose")
+    elif user_score == 21:
+        print("You got 21.\nYou Win")
+    elif dealer_score == 21:
+        print("The Dealer got 21.\nYou Lose")
+    elif user_score > dealer_score and dealer_score < 21:
+        print("You got higher than the Dealer.\nYou Win")
+    elif dealer_score > 21:
+        print("The Dealer went over.\nYou Win")
+    elif dealer_score > user_score:
+        print("The Dealer got higher than you.\nYou Lose")
 
 game()
